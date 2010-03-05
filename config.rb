f7f2a0ce1fff7d0a65f6669533a3205407ce4212
Adam Stacoviak <adam@gethandcrafted.com>
2010-03-04 22:49:35 -0600

    Updated Sass output to compact, broke out the reset to its own file and use the standard CSS import in the build, added the CSS3 mixins from Compass pre as well as Grid Coordinates to the Sass library, moved shared layout partials to shared/layout

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 ROOT = File.join(File.dirname(__FILE__), '/')
 puts "Site root is: " + File.expand_path(ROOT)
 
-output_style = :compressed
+output_style = :compact
 project_path = ROOT               # must be set for Compass to work 
 sass_dir     = "src/stylesheets"  # dir containing Sass / Compass source files
 http_path    = "/"                # root when deployed
