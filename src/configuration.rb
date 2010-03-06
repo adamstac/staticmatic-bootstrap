d4c77615473f0e6c30d250b312da8c98d16557a2
Adam Stacoviak <adam@gethandcrafted.com>
2010-03-06 16:12:32 -0600

    Updated the staticmatic config to be more verbose and helpful

--- src/configuration.rb
+++ src/configuration.rb
@@ -1,11 +1,25 @@
 require 'compass'
 
+# Parse the Compass config
 Compass.configuration.parse('config.rb')
+
+# Default is 3000
+configuration.preview_server_port = 3040
+
+# Default is localhost
+configuration.preview_server_host = "localhost"
+
+# Default is true
+# When false .html & index.html get stripped off generated urls
+configuration.use_extensions_for_page_links = true
+
+# Default is an empty hash
+# We use Compass's config
 configuration.sass_options = Compass.sass_engine_options
 
+# Default is an empty hash
+# http://haml-lang.com/docs/yardoc/file.HAML_REFERENCE.html#options
 configuration.haml_options = {
-  # :format => :html5,
+  #:format => :html5,
   :attr_wrapper => '"'
 }
-
-# configuration.use_extensions_for_page_links = false
