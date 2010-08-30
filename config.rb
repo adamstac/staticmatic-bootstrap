941ac8a9fd2bd6508acc8bb452ffbc1ea9224a8b
Adam Stacoviak <adam@adamstacoviak.com>
2010-08-30 11:43:56 -0500

    Set line_comments to false by default

--- config.rb
+++ config.rb
@@ -1,13 +1,14 @@
 ROOT = File.join(File.dirname(__FILE__), '/')
 puts "Site root is: " + File.expand_path(ROOT)
 
-output_style = :expanded
-project_path = ROOT               # must be set for Compass to work 
-sass_dir     = "src/stylesheets"  # dir containing Sass / Compass source files
-http_path    = "/"                # root when deployed
-css_dir      = "site/stylesheets" # final CSS
-images_dir   = "site/images"      # final images
-http_images_path = "/images"
+output_style        = :expanded
+project_path        = ROOT               # must be set for Compass to work 
+sass_dir            = "src/stylesheets"  # dir containing Sass / Compass source files
+http_path           = "/"                # root when deployed
+css_dir             = "site/stylesheets" # final CSS
+images_dir          = "site/images"      # final images
+http_images_path    = "/images"
+line_comments       = false
 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
