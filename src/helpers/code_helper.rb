12b2352e8bec8391a4b0d8ee539a0fa13e9e9fe4
Adam Stacoviak <adam@gethandcrafted.com>
2010-09-21 00:01:40 -0500

    Updated apotonicks CodeHelper

--- src/helpers/code_helper.rb
+++ src/helpers/code_helper.rb
@@ -3,9 +3,9 @@ require "grit"
 module CodeHelper
   def code(tag, file)
     
-    r = Grit::Repo.new('/home/nick/projects/peters-guide')
-    c = r.tag(tag).commit
+    repo = Grit::Repo.new(repo_name)
+    commit = repo.tag(tag).commit
     
-    CodeRay.scan((c.tree/file).data, :ruby).html(:line_numbers => :inline).div
+    CodeRay.scan((commit.tree/file).data, :ruby).html(:line_numbers => :inline).div
   end
 end
